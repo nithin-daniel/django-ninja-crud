f7a8f866b193534d71f836d02e7d4f25680f3ac8
nithindaniel1 <nithindaniel2018@gmail.com>
2024-01-06 16:19:13 +0530

    updated build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,7 @@
 echo "BUILD START"
 python3.9 -m pip install -r requirements.txt
+# Apply database migrations
+echo "Applying database migrations"
 python3.9 manage.py migrate
+echo "Done all database migrations"
 echo "BUILD END"
